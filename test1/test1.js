817cefb6838b4ab942caf2cc1a0489f87816b258
BronyaZaychikl <email@example.com>
2019-07-23 20:57:25 +0800

    js1去重

--- test1/test1.js
+++ test1/test1.js
@@ -1,42 +1,59 @@
 
 function aaa() {
-    var arr = document.getElementsByTagName("p");
-    var col = [];
-    for (var i = 0, n; n = arr[i]; ++i) {
-        col.push(n);
+    var arr = document.getElementsByTagName("p");//取三个随机格子
+    var ass=[];
+    for(var i=0;i<9;i++){
+      ass[i]=arr[i];
     }
     var result = [];
     var ranNum = 3;
     for (var i = 0; i < ranNum; i++) {
-        var ran = Math.floor(Math.random() * (col.length - i));
-        result.push(col[ran]);
-        col[ran] = col[col.length - i - 1];
+        var ran = Math.floor(Math.random() * (ass.length - i));
+        result.push(ass[ran]);
+        ass[ran] = ass[ass.length - i - 1];
     }
+console.log(result);
     for (var i = 0; i < 9; i++) {//每次随机颜色时遍历将背景设置好
         document.getElementsByTagName('p')[i].style.backgroundColor = "orange";
     }
-    result[0].style.backgroundColor = randomColor();
-    result[1].style.backgroundColor = randomColor();
-    result[2].style.backgroundColor = randomColor();
-    function randomColor() {
-        var col = "#";
-        for (var i = 0; i < 6; i++) {
-            col += Math.floor(Math.random() * 16).toString(16);
+    var ax = [];
+    for (var x = 0; x < 3; x++) {
+        function aax() {//随机颜色
+            var col = "#";
+            for (var i = 0; i < 6; i++) {
+                col += Math.floor(Math.random() * 16).toString(16);
+            }
+            return col;
         }
-        return col;
+        ax[x] = aax();
+        if (x > 0) {
+            for (var t = 0; t <= x - 1; t++) {
+                if (ax[x] == ax[t]) {
+                    x--;
+                }
+            }
+        }
     }
+    console.log(ax[0]);
+    console.log(ax[1]);
+    console.log(ax[2]);
+    result[0].style.backgroundColor = ax[0];
+    result[1].style.backgroundColor = ax[1];
+    result[2].style.backgroundColor = ax[2];
 }
+
 var btn1 = document.getElementsByTagName("button")[0];
 var btn2 = document.getElementsByTagName("button")[1];
-var aq;
+var aq;//重复
 function abc() {
     aq = window.setInterval("aaa()", 500);
     btn1.disabled = true;
     btn1.style.backgroundColor = "orange";
     btn2.style.backgroundColor = "#fff";
-    btn1.style.color = "#333";
+    btn1.style.color = "#fff";
+    btn2.style.color = "orange";
 }
-function abd() {
+function abd() {//停止重复
     window.clearInterval(aq);
     for (var i = 0; i < 9; i++) {//每次随机颜色时遍历将背景设置好
         document.getElementsByTagName('p')[i].style.backgroundColor = "orange";
@@ -44,4 +61,6 @@ function abd() {
     btn1.disabled = false;
     btn2.style.backgroundColor = "orange";
     btn1.style.backgroundColor = "#fff";
+    btn1.style.color = "orange";
+    btn2.style.color = "#fff";
 }
