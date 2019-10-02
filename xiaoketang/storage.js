a0cb33c16d21daa74b3d44e70c95501d2008ac64
BronyaZaychikl <email@example.com>
2019-10-02 20:32:42 +0800

    小课堂

---
+++ xiaoketang/storage.js
@@ -0,0 +1,26 @@
+//sessionStorage 示例  (保存一个会话周期:从打开浏览器——到关闭浏览器窗口)
+function saveStorage(id){
+    var target=document.getElementById(id);
+    var str=target.value;
+    sessionStorage.setItem("message",str);
+    //或者sessionStorage.message=str;
+}
+function loadStorage(id){
+    var target=document.getElementById(id);
+    var msg=sessionStorage.getItem("message");
+    //或者var msg=sessionStorage.message;
+    target.innerHTML=msg;
+}
+// //localStorage 示例(可永久保存)    
+// function saveStorage(id){
+//     var target=document.getElementById(id);
+//     var str=target.value;
+//     localStorage.setItem("message",str);
+//     //或者localStorage.message=str;
+// }
+// function loadStorage(id){
+//     var target=document.getElementById("msg");
+//     var msg=localStorage.getItem("message");
+//     //或者var msg=localStorage.message;
+//     target.innerHTML=msg;
+// }
